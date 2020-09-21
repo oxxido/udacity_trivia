15e54402775c9db3674a39073ee4c3cd561b6f61
Ger <oxxido@gmail.com>
2020-09-21 17:12:29 -0300

    starter code updated

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -24,7 +24,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+  
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +33,7 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
 
 
 # Make the tests conveniently executable
