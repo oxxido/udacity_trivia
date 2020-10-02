64fdeacd5ea23b26386c5081731368e2da895c4b
Ger <oxxido@gmail.com>
2020-10-02 12:02:07 -0300

    starting to write tests

---
+++ Enhacements.md
@@ -0,0 +1,18 @@
+# Full Stack API Final Project
+
+## Frontend
+
+- I added a CSS framework to help some specific ui details i like to improve
+- Changed the size, position and aligment of the menu
+- Created (downloaded) a logo
+- Changed sizes, spacing, cursor style, etc
+- Reworked each question on "list", changed position, added more style
+- Modified the search location and style
+- Modified the add new trivia
+
+
+## Backend
+
+- Requirements asked to "... POST endpoint to get questions based on a search term." but i prefer to use CRUD recommendations and add the search to the GET 
+endpoint, because I manage to create an entire new behaviour (search question)
+by just adding a couple of lines of code.
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -39,8 +39,86 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['total_books'])
-        self.assertTrue(len(data['books']))
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['categories']), 6)
+
+    def test_post_categories(self):
+        res = self.client().post('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 405)
+        self.assertEqual(data['message'], 'Method not allowed')
+
+    def test_get_questions(self):
+        res = self.client().get('/questions?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['current_page'], 1)
+        self.assertTrue(len(data['questions']) > 0)
+        self.assertTrue(data['total_questions'] > 0)
+        self.assertEqual(len(data['categories']), 6)
+
+    def test_page_out_of_bounds(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_search_with_results(self):
+        res = self.client().get('/questions?question=to')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['current_page'], 1)
+        self.assertEqual(len(data['categories']), 6)
+        self.assertTrue(len(data['questions']) > 0)
+        self.assertTrue(data['total_questions'] > 0)
+
+    def test_search_with_results_page_2(self):
+        res = self.client().get('/questions?question=a&page=2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['current_page'], 2)
+        self.assertEqual(len(data['categories']), 6)
+        self.assertTrue(len(data['questions']) > 0)
+        self.assertTrue(data['total_questions'] > 0)
+    
+    def test_search_with_results_out_of_bounds(self):
+        res = self.client().get('/questions?question=a&page=20')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_search_with_no_results(self):
+        res = self.client().get('/questions?question=jacaranda')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_search_with_no_results_and_out_of_bounds(self):
+        res = self.client().get('/questions?question=jacaranda&page=2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Resource not found')
 
     def test_json_404_return(self):
         res = self.client().get('/non_existing_page')
